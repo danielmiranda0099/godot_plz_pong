d44a6e4eb9b12bd366fd3c96f2d00be049333492
Daniel Master <DanielMaster09@gmail.com>
2023-04-24 07:55:31 -0500

    [ADD] Movement Ball

--- Player.gd
+++ Player.gd
@@ -3,6 +3,7 @@ extends KinematicBody2D
 var speed = 450
 var direction = Vector2.ZERO
 
+# warning-ignore:unused_argument
 func _physics_process(delta):
 	
 	if Input.is_action_pressed("ui_up"):
@@ -11,6 +12,7 @@ func _physics_process(delta):
 	if Input.is_action_pressed("ui_down"):
 		direction.y += 1
 		
+# warning-ignore:return_value_discarded
 	move_and_slide(direction * speed)
 	
 	direction = Vector2.ZERO
