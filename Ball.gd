f167c14035dbe8fb8024a8cd20ba7fb04f33379d
danielmiranda0099 <danielmesa877@gmail.com>
2023-04-27 22:35:31 -0500

    [ADD] Filter Collision layers with class_name

--- Ball.gd
+++ Ball.gd
@@ -1,4 +1,5 @@
 extends KinematicBody2D
+class_name Ball
 
 var speed = 0
 var direction = Vector2.ZERO
--- Level.gd
+++ Level.gd
@@ -1,5 +1,11 @@
 extends Node2D
 
+var playerScore = 0
+var enemyScore = 0
+
+func _process(_delta):
+	$"Marcador Player".text = str(playerScore) 
+	$"Marcador Enemy".text = str(enemyScore)
 
 func _ready():
 	pass
@@ -12,9 +18,13 @@ func _restart_game():
 
 	$Ball.reset_ball()
 
-func _on_Arco_Player_body_entered(_body):
-	_restart_game()
+func _on_Arco_Player_body_entered(body):
+	if body is Ball:
+		enemyScore += 1
+		_restart_game()
 
 
-func _on_Arco_Enemy_body_entered(_body):
-	_restart_game()
+func _on_Arco_Enemy_body_entered(body):
+	if body is Ball:
+		playerScore += 1
+		_restart_game()
